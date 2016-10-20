c20cd4d7b131f8761b6a361a12c46e56a9e077a6
Nicholas G Barrs <nbarrs@joey7.cs.clemson.edu>
2016-10-20 01:44:29 -0400

    "fixed ahsoka reading flags. previous commit actually only worked for up to 10  messages. now works for up to 100. hopefully he won't test with more than that. also now gives error if a message is longer than 80 chars."

--- ahsoka.c
+++ ahsoka.c
@@ -1,6 +1,5 @@
 #include "starLord.h"
-#include <string.h>
-#define NUM_MSGS 15
+#define NUM_MSGS 64
 
 int main(int argc, char *argv[]) {
 
@@ -17,7 +16,9 @@ int main(int argc, char *argv[]) {
 	char *hostHeader; 			// host header
 	char *messages[NUM_MSGS];	// array of messages to send to server
 	int msgsParsed = 0;			// number of msgs parsed so far from cmnd line
-	
+	bool validMsgFlag = true;
+	bool validMsgLen = true;
+
 	// initialize messages array with NUM_MSGS strings of 80 chars each
 	int i;
 	for (i=0; i<NUM_MSGS; i++) 
@@ -29,32 +30,48 @@ int main(int argc, char *argv[]) {
 		switch (argv[j][1]) {
 			case 's':
 				servIP = argv[j+1];
-				printf("got s, servIP is %s\n", servIP);
 				break;
 			case 'p':
 				servPort = argv[j+1];
-				printf("got p, servPort is %s\n", servPort);
 				break;
 			case 'h':
 				hostHeader = argv[j+1];
-				printf("got h, hostHeader is %s\n", hostHeader);
 				break;
 			case 'a':
-				if (((int)argv[j][2]-48) != (msgsParsed+1)) {
-					printf("\n\nERROR with message flag -a%c!\n", (char)argv[j][2]);
+				if (argv[j][3]-48 >= 0 && argv[j][3]-48 <= 9) {
+					if (((int)(argv[j][2])-48) != ((msgsParsed+1)/10) ||
+						((int)(argv[j][3])-48) != ((msgsParsed+1)%10)) {
+							validMsgFlag = false;
+					}
+				}
+				else if (((int)(argv[j][2])-48 != (msgsParsed+1))) {
+					validMsgFlag = false;
+				}
+				if (!validMsgFlag) {
+					printf("\nERROR with message flag order!\n");
 					printf("messages must be sent on command line corresponding to");
 					printf(" -aX flags,\nwhere X is an integer. the messages must");
 					printf(" be sent in order that they wish\nto be sent; thus ");
 					printf("the -aX flags must occur in ascending order, starting ");
 					printf("with -a1.\nTry again.\n\n");
+					exit(1);
+				}
+				else {
+					if (strlen(argv[j+1]) > 80) {
+						validMsgLen = false;
+					}
+				}
+				if (!validMsgLen) {
+					printf("\nERROR with mesage length!\n");
+					printf("max length of message is 80 chars. Try again\n\n");
+					exit(1);
 				}
 				else {
 					messages[msgsParsed++] = argv[j+1];
-					printf("got a%c, message%c is %s\n", (char)argv[j][2], (char)argv[j][2], messages[msgsParsed-1]);
+					break;
 				}
-				break;
 			default:
-				if (argv[j][0] != '-') {
+				if ((char)argv[j][0] != '-') {
 					printf("Syntax: ./ahsoka -s <server addr> -p <port> ");
 					printf("-h <hostHeader> -a <message1>...-aN <messageN>\n");
 				}
@@ -62,11 +79,21 @@ int main(int argc, char *argv[]) {
 					printf("only acceptable flags are -s, -p, -h, and -aX, ");
 					printf("where X is an integer greater than 1.\n");
 				}
-				break;
+				exit(1);
 		}
 		j+=2;
 	}
 
+	// Print captured strings to make sure parsing occurred correctly
+	printf("\n\nservIP is %s\n", servIP);
+	printf("servPort is %s\n", servPort);
+	printf("hostHeader is %s\n", hostHeader);
+	printf("the messages to be sent are as follows:\n");
+	int m;
+	for (m=0; m<msgsParsed; m++) {
+		printf("message%d: %s\n", m+1, messages[m]);
+	}
+	printf("\n");
 
 	/*
 	// Parse command line arguments with flags and initialize variables
